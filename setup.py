c0ae15686f0fc786b0c4dd26dcbd36f5b50f9c62
Nick Lee <lee1nick@yahoo.ca>
2015-09-16 00:29:51 +0800

    Version 2.50 instead of 2.5

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.5',
+    version='2.50',
 
     description='Python wrapper for Telegram Bot API',
 
