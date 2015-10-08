2efc677477b289a5ee258180384ecfe0bc080e50
Nick Lee <lee1nick@yahoo.ca>
2015-10-08 22:40:49 +0800

    Version 3.1

--- setup.py
+++ setup.py
@@ -66,9 +66,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='3.0',
+    version='3.1',
 
-    description='Python wrapper for Telegram Bot API',
+    description='Python framework for Telegram Bot API',
 
     long_description=long_description,
 
