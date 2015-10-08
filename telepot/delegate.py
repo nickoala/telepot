07b52d20edde193979f19386dfae285c3dfb5dd5
Nick Lee <lee1nick@yahoo.ca>
2015-10-08 22:40:12 +0800

    Added `per_chat_id_except()`

--- telepot/delegate.py
+++ telepot/delegate.py
@@ -5,6 +5,9 @@ def per_chat_id():
 def per_chat_id_in(s):
     return lambda msg: msg['chat']['id'] if msg['chat']['id'] in s else None
 
+def per_chat_id_except(s):
+    return lambda msg: msg['chat']['id'] if msg['chat']['id'] not in s else None
+
 def call(func, *args, **kwargs):
     def f(seed_tuple):
         return func, (seed_tuple,)+args, kwargs
