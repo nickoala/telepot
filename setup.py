637554569bd1bb2a498ef27da328e03bd6e3f7c6
Nick Lee <lee1nick@yahoo.ca>
2015-10-29 15:59:14 +0800

    Version 4.0

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='3.2',
+    version='4.0',
 
     description='Python framework for Telegram Bot API',
 
