316f7bcf51cce1d7d380829c52913030c5f7769b
Nick Lee <lee1nick@yahoo.ca>
2015-10-30 17:35:20 +0800

    MessageCounter example for asyncio

---
+++ examples/countera.py
@@ -0,0 +1,28 @@
+import sys
+import asyncio
+import telepot
+from telepot.delegate import per_chat_id
+from telepot.async.delegate import create_open
+
+class MessageCounter(telepot.helper.ChatHandler):
+    def __init__(self, seed_tuple, timeout):
+        super(MessageCounter, self).__init__(seed_tuple, timeout)
+        self._count = 0
+
+    @asyncio.coroutine
+    def on_message(self, msg):
+        self._count += 1
+        yield from self.sender.sendMessage(self._count)
+
+TOKEN = sys.argv[1]  # get token from command-line
+
+bot = telepot.async.DelegatorBot(TOKEN, [
+    (per_chat_id(), create_open(MessageCounter, timeout=10)),
+])
+
+loop = asyncio.get_event_loop()
+loop.create_task(bot.messageLoop())
+print('Listening ...')
+
+loop.run_forever()
+
