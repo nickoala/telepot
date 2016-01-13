044c22fbaa3ba56f2ebbce01ffb6edd07b7319ca
Nick Lee <lee1nick@yahoo.ca>
2016-01-13 15:00:32 +0800

    Added a few per_XXX functions to deal with inline query

--- telepot/delegate.py
+++ telepot/delegate.py
@@ -1,12 +1,42 @@
+from telepot import flavor
+
+def _wrap_none(fn):
+    def w(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except KeyError:
+            return None
+    return w
 
 def per_chat_id():
-    return lambda msg: msg['chat']['id']
+    return _wrap_none(lambda msg: msg['chat']['id'])
 
 def per_chat_id_in(s):
-    return lambda msg: msg['chat']['id'] if msg['chat']['id'] in s else None
+    return _wrap_none(lambda msg: msg['chat']['id'] if msg['chat']['id'] in s else None)
 
 def per_chat_id_except(s):
-    return lambda msg: msg['chat']['id'] if msg['chat']['id'] not in s else None
+    return _wrap_none(lambda msg: msg['chat']['id'] if msg['chat']['id'] not in s else None)
+
+def per_from_id():
+    return _wrap_none(lambda msg: msg['from']['id'])
+
+def per_from_id_in(s):
+    return _wrap_none(lambda msg: msg['from']['id'] if msg['from']['id'] in s else None)
+
+def per_from_id_except(s):
+    return _wrap_none(lambda msg: msg['from']['id'] if msg['from']['id'] not in s else None)
+
+def _isinline(msg):
+    return flavor(msg) in ['inline_query', """'chosen_inline_result'"""]
+
+def per_inline_from_id():
+    return _wrap_none(lambda msg: msg['from']['id'] if _isinline(msg) else None)
+    
+def per_inline_from_id_in(s):
+    return _wrap_none(lambda msg: msg['from']['id'] if _isinline(msg) and msg['from']['id'] in s else None)
+
+def per_inline_from_id_except(s):
+    return _wrap_none(lambda msg: msg['from']['id'] if _isinline(msg) and msg['from']['id'] not in s else None)
 
 def call(func, *args, **kwargs):
     def f(seed_tuple):
