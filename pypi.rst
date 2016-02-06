411d8efc4e952a9440a442d145115e4a91830a29
Nick Lee <lee1nick@yahoo.ca>
2016-02-06 13:52:55 +0800

    Version 6.3

--- pypi.rst
+++ pypi.rst
@@ -1,4 +1,4 @@
 .
 
-`A lot of examples and documentations on Github » <https://github.com/nickoala/telepot>`_
+`Lots of examples and documentations on Github » <https://github.com/nickoala/telepot>`_
 -----------------------------------------------------------------------------------------
--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='6.2',
+    version='6.3',
 
     description='Python framework for Telegram Bot API',
 
