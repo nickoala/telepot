eb2bcfd12ee68e4c4233b6d37e578513d37649f3
Nick Lee <lee1nick@yahoo.ca>
2016-02-16 15:16:55 +0800

    Aliased functions `per_ZZZ()`

--- telepot/async/delegate.py
+++ telepot/async/delegate.py
@@ -1,4 +1,20 @@
 import asyncio
+import telepot.delegate
+from telepot.helper import WaitTooLong, StopListening
+from telepot.async.helper import _yell
+
+# aliases for convenience
+per_chat_id = telepot.delegate.per_chat_id
+per_chat_id_in = telepot.delegate.per_chat_id_in
+per_chat_id_except = telepot.delegate.per_chat_id_except
+
+per_from_id = telepot.delegate.per_from_id
+per_from_id_in = telepot.delegate.per_from_id_in
+per_from_id_except = telepot.delegate.per_from_id_except
+
+per_inline_from_id = telepot.delegate.per_inline_from_id
+per_inline_from_id_in = telepot.delegate.per_inline_from_id_in
+per_inline_from_id_except = telepot.delegate.per_inline_from_id_except
 
 def call(corofunc, *args, **kwargs):
     def f(seed_tuple):
@@ -17,25 +33,26 @@ def create_open(cls, *args, **kwargs):
 
         @asyncio.coroutine
         def wait_loop():
-            @asyncio.coroutine
-            def invoke(func, *a):
-                if asyncio.iscoroutinefunction(func):
-                    return (yield from func(*a))
-                else:
-                    return func(*a)
-
             bot, msg, seed = seed_tuple
             try:
-                handled = yield from invoke(j.open, msg, seed)
+                handled = yield from _yell(j.open, msg, seed)
                 if not handled:
-                    yield from invoke(j.on_message, msg)
+                    yield from _yell(j.on_message, msg)
 
                 while 1:
                     msg = yield from j.listener.wait()
-                    yield from invoke(j.on_message, msg)
+                    yield from _yell(j.on_message, msg)
 
+            # These exceptions are "normal" exits.
+            except (WaitTooLong, StopListening) as e:
+                yield from _yell(j.on_close, e)
+
+            # Any other exceptions are accidents. **Print it out.**
+            # This is to prevent swallowing exceptions in the case that on_close()
+            # gets overridden but fails to account for unexpected exceptions.
             except Exception as e:
-                yield from invoke(j.on_close, e)
+                traceback.print_exc()
+                yield from _yell(j.on_close, e)
 
         return wait_loop()
     return f
